d23b2ad75f00626e713d61e40a04b7d083c7db63
YasuHirata <hirata@fancs.jp>
2020-09-29 09:36:39 +0000

    laravel12

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,12 @@ Route::group(['prefix' => 'admin'], function() {
 Route::get('admin/profile/create', 'Admin\ProfileController@add');
 
 Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@add')->middleware('auth');
+});
