baaf9d6f7ba07c88772bf25708192cd28677369d
YasuHirata <hirata@fancs.jp>
2020-09-30 07:46:21 +0000

    laravel14

--- routes/web.php
+++ routes/web.php
@@ -15,36 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::group(['prefix' => 'admin'], function() {
-//    Route::get('news/create', 'Admin\NewsController@add');
-// });
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-});
-
-// Route::get('XXX', 'AAAController@bbb');
-
-// Route::get('admin/profile/create', 'Admin\ProfileController@add');
-
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
-
-// Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
-
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Route::group(['prefix' => 'admin'], function() {
-    //Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    //Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    //Route::get('profile/edit', 'Admin\ProfileController@add')->middleware('auth');
-//});
-
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
-     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+     Route::post('news/create', 'Admin\NewsController@create'); 
+     //
      Route::post('profile/create', 'Admin\ProfileController@create');
      Route::post('profile/edit', 'Admin\ProfileController@update');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
